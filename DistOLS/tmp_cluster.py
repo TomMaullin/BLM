48721af2369a5a0f4defd457dabc7a72057ae7ce
TomMaullin <TomMaullin@gmail.com>
2018-11-02 16:08:59 +0000

    DistOLS/tmp_cluster.py

--- DistOLS/tmp_cluster.py
+++ DistOLS/tmp_cluster.py
@@ -7,9 +7,9 @@ import numpy as np
 def main():
 
     # Read in the list of NIFTI's needed for this OLS.
-    analydir = '/well/nichols/users/kfh142/data';
-    os.chdir(analydir);
-    Y_files = glob.glob("IMAGEN/spmstatsintra/*/SessionB/EPI_short_MID/swea/con_0010.nii")
+    analydir = '/well/nichols/users/kfh142/data'
+    os.chdir(analydir)
+    Y_files = glob.glob(os.path.join(analydir, "IMAGEN/spmstatsintra/*/SessionB/EPI_short_MID/swea/con_0010.nii"))
 
     # Design matrix and number of parameters.
     X = np.ones([1815, 1])
