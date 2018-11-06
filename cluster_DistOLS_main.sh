32957c0054245d03f1e3768cec3429c9351a618b
TomMaullin <tommaullin@gmail.com>
2018-11-06 16:24:43 +0000

    analysis now runs smoothly

--- cluster_DistOLS_main.sh
+++ cluster_DistOLS_main.sh
@@ -1,6 +1,20 @@
+rm DistOLS/binputs/*
+rm log/*
+
 qsub -N setup cluster_DistOLS_setup.sh
 
-nb=$(ls -1q DistOLS/binputs/Y* | wc -l)
+echo "Setting up distributed analysis..."
+
+# This loop waits for the setup job to finish before
+# deciding how many batches to run.
+nb=0
+while [ $nb -lt 1 ]
+do
+  sleep 1
+  if [ "$(ls -A DistOLS/binputs/)" ]; then
+    nb=$(ls -1q DistOLS/binputs/Y* | wc -l)
+  fi
+done
 
 i=1
 while [ "$i" -le "$nb" ]; do
