4b8ad7b3e0adc14e4c939f920d6c13ca6e8d14fd
TomMaullin <tommaullin@gmail.com>
2018-12-06 19:37:59 +0000

    wip python local version

--- BLM/blm_main.py
+++ BLM/blm_main.py
@@ -5,14 +5,37 @@ import numpy as np
 import subprocess
 import warnings
 import resource
-from lib import blkXtX, blkXtY
+import blm_setup
+import blm_batch
+import blm_concat
 
 def main(Y_files, X): #flag='spat'
 
-    # This will be the function to call this as a python package
-    # plan - call blm_setup
-    #        loop through each batch using blm_batch
-    #        call blm_concat
+    print('Setting up analysis...')
+	# Run the setup job to obtain the number of batches needed.
+    blm_setup.main()
+
+    # Load in inputs
+    with open('blm_defaults.yml', 'r') as stream:
+        inputs = yaml.load(stream)
+
+    # Retrieve Output directory
+    OutDir = inputs['outdir']
+
+    # Load nB
+    with open(os.path.join(OutDir, "nb.txt"), 'w') as f:
+        nB = int(f.readline())
+
+    # Run batch jobs
+    for i in range(0, nB):
+    	print('Running job ' + str(i) + '/' + str(nB))
+    	blm_batch.main(i+1)
+
+    # Run concatenation job
+    print('Combining batch results...')
+	blm_concat.main()
+
+	print('Distributed analysis complete. Please see "' + outDir + "for output.")
 
 if __name__ == "__main__":
     main()
