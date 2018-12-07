c8a53ebf93d80c8f494dc7a0a48def871306518e
TomMaullin <tommaullin@gmail.com>
2018-12-07 11:57:11 +0000

    running locally now works

---
+++ local_blm_main.py
@@ -0,0 +1,39 @@
+import numpy as np
+import yaml
+import os
+from BLM import blm_setup
+from BLM import blm_batch
+from BLM import blm_concat
+
+def main():
+
+    print('Setting up analysis...')
+    # Run the setup job to obtain the number of batches needed.
+    blm_setup.main()
+
+    print(os.getcwd())
+    # Load in inputs
+    with open(os.path.join(os.getcwd(),'blm_defaults.yml'), 'r') as stream:
+        print(os.path.join(os.getcwd(),'blm_defaults.yml'))
+        inputs = yaml.load(stream)
+
+    # Retrieve Output directory
+    OutDir = inputs['outdir']
+
+    # Load nB
+    with open(os.path.join(OutDir, "nb.txt"), 'r') as f:
+        nB = int(f.readline())
+
+    # Run batch jobs
+    for i in range(0, nB):
+        print('Running job ' + str(i) + '/' + str(nB))
+        blm_batch.main(i+1)
+
+    # Run concatenation job
+    print('Combining batch results...')
+    blm_concat.main()
+
+    print('Distributed analysis complete. Please see "' + OutDir + "for output.")
+
+if __name__ == "__main__":
+    main()
