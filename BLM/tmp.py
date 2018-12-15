e740d9b10a499ac3d430573b62c8e79b9e070e79
TomMaullin <tommaullin@gmail.com>
2018-12-15 17:45:18 +0000

    starting point for plot code

--- BLM/tmp.py
+++ BLM/tmp.py
@@ -5,23 +5,22 @@ import nibabel as nib
 def main():
 
     print('running')
-    Y_files = glob.glob('gpfs2/well/nichols/projects/UKB/MNI/*_tfMRI_cope1_MNI.nii.gz')
-    print(Y_files)
+    Y_files = glob.glob('/gpfs2/well/nichols/projects/UKB/MNI/*_tfMRI_cope1_MNI.nii.gz')
 
     for Y_file in Y_files:
-
-        print(Y_file)
 
         Y = nib.load(Y_file)
         d = Y.get_data()
         d = np.nan_to_num(d)
 
+        d = d[0:d.shape[0],0:d.shape[1],int(np.floor(d.shape[2]/2)):d.shape[2]]
+
+
         frac = np.count_nonzero(d)/(d.shape[0]*d.shape[1]*d.shape[2])
 
-        print(frac)
-
-        if frac > 0.3:
+        if frac < 0.7:
             print(Y_file)
+            print(frac)
 
 if __name__ == "__main__":
     main()
