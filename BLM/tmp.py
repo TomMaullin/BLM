4629aa3fed39dd658e9fef1089cdb754a360c6cb
TomMaullin <tommaullin@gmail.com>
2018-12-15 14:51:58 +0000

    tmp added to verify inputs

--- BLM/tmp.py
+++ BLM/tmp.py
@@ -2,21 +2,28 @@ import glob
 import numpy as np
 import nibabel as nib
 
-Y_files = glob.glob('gpfs2/well/nichols/projects/UKB/MNI/*_tfMRI_cope1_MNI.nii.gz')
+def main():
 
-for Y_file in Y_files:
+    print('running')
+    Y_files = glob.glob('gpfs2/well/nichols/projects/UKB/MNI/*_tfMRI_cope1_MNI.nii.gz')
+    print(Y_files)
 
-    print(Y_file)
+    for Y_file in Y_files:
 
-    Y = nib.load(Y_file)
-    d = Y.get_data()
-    d = np.nan_to_num(d)
-
-    frac = np.count_nonzero(d)/(d.shape[0]*d.shape[1]*d.shape[2])
-
-    print(frac)
-
-    if frac > 0.3:
         print(Y_file)
+
+        Y = nib.load(Y_file)
+        d = Y.get_data()
+        d = np.nan_to_num(d)
+
+        frac = np.count_nonzero(d)/(d.shape[0]*d.shape[1]*d.shape[2])
+
+        print(frac)
+
+        if frac > 0.3:
+            print(Y_file)
+
+if __name__ == "__main__":
+    main()
 
 
