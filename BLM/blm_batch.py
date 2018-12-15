c9f3b827aa1276048017c51e86e0bdd7d65b36a4
TomMaullin <tommaullin@gmail.com>
2018-12-15 21:56:04 +0000

    wip

--- BLM/blm_batch.py
+++ BLM/blm_batch.py
@@ -32,7 +32,11 @@ def main(batchNo):
 
             Y_files.append(line.replace('\n', ''))
 
-    X = pandas.io.parsers.read_csv(inputs['X'], sep=',').values
+    X = pandas.io.parsers.read_csv(
+        inputs['X'], sep=',').values
+    
+    if X.shape[0]==0:
+        X = X.reshape([1,X.shape[1]])
 
     SVFlag = inputs['SVFlag']
     OutDir = inputs['outdir']
