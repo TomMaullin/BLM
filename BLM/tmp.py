35b561032441ecb077974ef1217b8056be7db49b
TomMaullin <tommaullin@gmail.com>
2018-12-15 17:55:27 +0000

    wip generating inputs

--- BLM/tmp.py
+++ BLM/tmp.py
@@ -1,26 +1,31 @@
 import glob
 import numpy as np
 import nibabel as nib
+import os
 
 def main():
 
     print('running')
     Y_files = glob.glob('/gpfs2/well/nichols/projects/UKB/MNI/*_tfMRI_cope1_MNI.nii.gz')
+    os.mkdir(os.path.join(os.getcwd(),'BLM','test','data','ukbb'))
 
+    j = 1
     for Y_file in Y_files:
 
-        Y = nib.load(Y_file)
-        d = Y.get_data()
-        d = np.nan_to_num(d)
+        for i in range(10, length(Y_files)+1):
 
-        d = d[0:d.shape[0],0:d.shape[1],int(np.floor(d.shape[2]/2)):d.shape[2]]
+            with open(os.path.join(os.getcwd(),'BLM','test','data','ukbb', 'Y_files_ukbb_' + str(i) + '.txt')) as a:
 
+                if j >= i:
 
-        frac = np.count_nonzero(d)/(d.shape[0]*d.shape[1]*d.shape[2])
+                    a.write(Y_file + os.linesep)
 
-        if frac < 0.7:
-            print(Y_file)
-            print(frac)
+            i = i+1
+
+        j = j+1
+
+    print('done')
+        
 
 if __name__ == "__main__":
     main()
