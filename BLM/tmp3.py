a2c5fad7dc3171dc1d271aa594aade0d6a9963c0
TomMaullin <TomMaullin@gmail.com>
2018-12-17 16:56:06 +0000

    time

--- BLM/tmp3.py
+++ BLM/tmp3.py
@@ -3,12 +3,13 @@ import numpy as np
 import nibabel as nib
 import os
 import pandas
+import matplotlib as plt
 
 def main():
 
     print('running')
-    tvals = np.zeros((120,1));
-    for i in range(10,130):
+    tvals = np.zeros((130,1));
+    for i in range(10,140):
     	#tvals = np.concatenate((tvals,
         #	pandas.io.parsers.read_csv('/gpfs2/well/nichols/users/inf852/t'+ str(10*i) + '.csv')), axis=0)
         t = np.loadtxt('/gpfs2/well/nichols/users/inf852/t'+ str(10*i) + '.csv')
@@ -16,7 +17,10 @@ def main():
         print(t.shape)
         tvals[i-10] = t
 
-    print(tvals)
+    np.savetxt(os.path.join(os.getcwd(),"tvals.csv"), 
+               t, delimiter=",") 
+
+
 
 if __name__ == "__main__":
     main()
