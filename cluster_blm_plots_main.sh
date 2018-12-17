b114869dcc7c39d31541bbdbd6d7c44d5f689a97
TomMaullin <TomMaullin@gmail.com>
2018-12-17 20:14:53 +0000

    wip

--- cluster_blm_plots_main.sh
+++ cluster_blm_plots_main.sh
@@ -1,10 +1,8 @@
 rm log/*
 
-i=10
-while [ "$i" -le 300 ]; do
-  qsub -N time$i -V ./cluster_blm_plots.sh $(( 10 * $i ))
+i=22
+while [ "$i" -le 52 ]; do
+  qsub -N time$i -V ./cluster_blm_plots.sh $(( $i ))
   i=$(($i + 1))
-  qstat
-  sleep $(( $i / 10 ))
   qstat
 done
--- local_blm_plots.py
+++ local_blm_plots.py
@@ -7,23 +7,33 @@ import shutil
 
 def main(i):
     
+    # inputsi = {'SVFlag': True,
+    #            'contrasts': [{'c1': {'vector': [1], 
+    #                                  'statType': 'T', 
+    #                                  'name': 'contrast1'}}], 
+    #            'outdir': '/well/nichols/users/inf852/' + str(i) + 'ukbbsv/', 
+    #            'X': '/users/nichols/inf852/BLM-py/BLM/test/data/ukbb/X_ukbb_' + str(i) + '.csv', 
+    #            'Y_files': '/users/nichols/inf852/BLM-py/BLM/test/data/ukbb/Y_files_ukbb_' + str(i) + '.txt', 
+    #            'M_files': '/users/nichols/inf852/BLM-py/BLM/test/data/ukbb/M_files_ukbb_' + str(i) + '.txt',
+    #            'MAXMEM': '2**31'}
+
     inputsi = {'SVFlag': True,
                'contrasts': [{'c1': {'vector': [1], 
                                      'statType': 'T', 
                                      'name': 'contrast1'}}], 
-               'outdir': '/well/nichols/users/inf852/' + str(i) + 'ukbbsv/', 
-               'X': '/users/nichols/inf852/BLM-py/BLM/test/data/ukbb/X_ukbb_' + str(i) + '.csv', 
-               'Y_files': '/users/nichols/inf852/BLM-py/BLM/test/data/ukbb/Y_files_ukbb_' + str(i) + '.txt', 
-               'M_files': '/users/nichols/inf852/BLM-py/BLM/test/data/ukbb/M_files_ukbb_' + str(i) + '.txt',
-               'MAXMEM': '2**31'}
+               'outdir': '/well/nichols/users/inf852/3000ukbbsv_mem' + str(i) + '/', 
+               'X': '/users/nichols/inf852/BLM-py/BLM/test/data/ukbb/X_ukbb_3000_mem' + str(i) + '.csv', 
+               'Y_files': '/users/nichols/inf852/BLM-py/BLM/test/data/ukbb/Y_files_ukbb_3000_mem' + str(i) + '.txt', 
+               'M_files': '/users/nichols/inf852/BLM-py/BLM/test/data/ukbb/M_files_ukbb_3000_mem' + str(i) + '.txt',
+               'MAXMEM': '2**' + str(i)}
 
     t1 = time.time()
     local_blm_main.main(inputsi)
     t2 = time.time()
     t=t2-t1
 
-    shutil.rmtree('/well/nichols/users/inf852/' + str(i) + 'ukbbsv/')
-    np.savetxt('/well/nichols/users/inf852/t' + str(i) + '.csv',np.array([t]), delimiter=",") 
+    shutil.rmtree('/well/nichols/users/inf852/3000ukbbsv_mem' + str(i) + '/')
+    np.savetxt('/well/nichols/users/inf852/t_mem' + str(i) + '.csv',np.array([t]), delimiter=",") 
 
 if __name__ == "__main__":
     main()
