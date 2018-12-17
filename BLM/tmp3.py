4f7d7ae8158b783d12545640718df64bcb5c00e6
TomMaullin <TomMaullin@gmail.com>
2018-12-17 16:35:10 +0000

    time

---
+++ BLM/tmp3.py
@@ -0,0 +1,18 @@
+import glob
+import numpy as np
+import nibabel as nib
+import os
+
+def main():
+
+    print('running')
+    tvals = [];
+    for i in range(10,130)
+    	tvals = np.concatenate((tvals,
+        	pandas.io.parsers.read_csv('/gpfs2/well/nichols/users/inf852/t'+ str(10*i) + '.csv'), axis=0)
+
+    print(tvals)
+
+if __name__ == "__main__":
+    main()
+
