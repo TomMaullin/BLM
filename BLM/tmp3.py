512ba5f801e9a6e3f747c7fc6096e98a798bdb82
TomMaullin <tommaullin@gmail.com>
2018-12-17 16:40:40 +0000

    wip tmp3

--- BLM/tmp3.py
+++ BLM/tmp3.py
@@ -7,7 +7,7 @@ def main():
 
     print('running')
     tvals = [];
-    for i in range(10,130)
+    for i in range(10,130):
     	tvals = np.concatenate((tvals,
         	pandas.io.parsers.read_csv('/gpfs2/well/nichols/users/inf852/t'+ str(10*i) + '.csv'), axis=0)
 
