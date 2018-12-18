03a855efddc23bc2395e49f1d6079d72474d30f6
TomMaullin <tommaullin@gmail.com>
2018-12-18 18:37:45 +0000

    tmp3 update

--- tmp3.py
+++ tmp3.py
@@ -4,11 +4,12 @@ import pandas as pd
 def main():
     Y_files = '/users/nichols/inf852/BLM-py/BLM/test/data/ukbb/Y_files_ukbb_6063.txt'
     X = np.zeros([len(Y_files),3])
-    tsvdat = pd.read_csv('/well/nichols/projects/UKB/SMS/ukb24729-fMRI-6063-demo.tsv')
+    tsvdat = pd.read_csv('/well/nichols/projects/UKB/SMS/ukb24729-fMRI-6063-demo.tsv', delimiter='\t')
 
     print(Y_files)
     print(X)
     print(tsvdat)
+    print(tsvdat.loc[tsvdat['eid']==5905827,'f.34.0.0'].values)
 
     for i in range(0, len(Y_files)):
 
