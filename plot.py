6cc1ee85c4e0e455e2e7b09c7f8622e7d9c1a660
TomMaullin <TomMaullin@gmail.com>
2018-12-18 10:02:57 +0000

    wip

--- plot.py
+++ plot.py
@@ -1,6 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-tvals = np.loadtxt('tvals.csv', delimiter=',')
-plt.plot(np.arange(10,1710,10),tvals)
+tvals = np.loadtxt('tvals1.csv', delimiter=',')
+for i in range(1,16):
+    tvals = tvals + np.loadtxt('tvals' + str(i) + '.csv', delimiter=',')
+print(len(np.arange(10,2920,10)))
+print(len(tvals))
+fig, ax = plt.subplots()
+ax.set_xlabel('Number of Subjects')
+ax.set_ylabel('Time (seconds)')
+plt.plot(np.arange(10,2920,10),tvals/15)
+
+np.savetxt('tvals_total_ns.csv', tvals/15)
 plt.show()
