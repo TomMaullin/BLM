d80589abd893bfff77d8b86de6fd0329f3919c8b
TomMaullin <tommaullin@gmail.com>
2019-01-02 12:20:29 +0000

    wip

--- cluster_blm_plots_main.sh
+++ cluster_blm_plots_main.sh
@@ -1,15 +1,14 @@
 rm log/*
 
-j=1
-while [ "$j" -le 15 ]; do
-  i=36
-  while [ "$i" -ge 26 ]; do
+j=8
+while [ "$j" -le 10 ]; do
+  i=10
+  while [ "$i" -le 15 ]; do
     echo $j
     qsub -N time_mem$i -V ./cluster_blm_plots.sh $(( $i )) $j
-    i=$(($i - 1))
+    i=$(($i + 1))
     qstat
-    sleep 10
   done
   j=$(($j + 1))
-  sleep $(( $j * 40 ))
+  sleep 1800
 done
