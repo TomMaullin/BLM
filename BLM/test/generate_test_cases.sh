359552bec5f0685130e9e2016b40607585f5615d
TomMaullin <tommaullin@gmail.com>
2019-02-07 11:35:24 +0000

    wip generate test data

---
+++ BLM/test/generate_test_cases.sh
@@ -0,0 +1,25 @@
+# Work out BLM dir
+BLMdir=$(realpath ../../)
+cd BLMdir
+
+# Read the test and data directories
+testdir=$1
+datadir=$2
+
+# Change the name of the test and data directories in the test configurations
+find ./BLM/test/cfg/*.yml -type f -exec sed -i 's/TEST_DIRECTORY/$testdir/g' {} \;
+find ./BLM/test/cfg/*.yml -type f -exec sed -i 's/DATA_DIRECTORY/$datadir/g' {} \;
+
+# Make a directory to store job ids if there isn't one already.
+mkdir -p ./BLM/test/cfgids
+
+# Run each test case
+i=1
+for cfg in $(ls ./BLM/test/cfg/*.yml)
+do
+  cfgfile=$(realpath $cfg)
+  # Run blm for test configuration and save the ids
+  bash ./blm_cluster.sh cfgfile > ./BLM/test/cfgids/testIDs$i
+
+  i=$(($i + 1))
+done
