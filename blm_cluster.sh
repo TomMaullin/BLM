4f4ac397112cb21946d4176953e4137936738b0e
Tommaullin <Tommaullin@gmail.com>
2020-10-18 14:03:21 +0100

    small fix for tests

--- blm_cluster.sh
+++ blm_cluster.sh
@@ -122,8 +122,12 @@ fi
 fsl_sub -j $resultsID -l log/ -N cleanup bash $BLM_PATH/scripts/cluster_blm_cleanup.sh $inputs > /tmp/$$ && cleanupID=$(awk 'match($0,/[0-9]+/){print substr($0, RSTART, RLENGTH)}' /tmp/$$)
 if [ "$cleanupID" == "" ] ; then
   echo "Clean up job submission failed!"
-else
+fi
+
+if [ "$printOpt" == "1" ] ; then
   echo "Submitting clean up job..."
   echo "Analysis submission complete. Please use qstat to monitor progress."
+else
+  echo $cleanupID
 fi
 
