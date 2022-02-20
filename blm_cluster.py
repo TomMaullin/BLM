1605863176a105dc052c1946f338ee9fd7e02e7c
Tom Maullin-Sapey <tommaullin@Caustic-Jam.local>
2022-02-20 02:25:01 +0000

    trying without dashboard

--- blm_cluster.py
+++ blm_cluster.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 
 	# Specify cluster setup
 	cluster = SGECluster(cores=1,
-	                     memory="100GB",
+	                     memory="80GB",
 	                     queue='short.qc',
 	                     walltime='01:00:00',
 	                     extra=['--no-dashboard'])
