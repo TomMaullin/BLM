24d15cd0925335d1f4eff4b8410db81248b4ac22
Tom Maullin-Sapey <tommaullin@Caustic-Jam.local>
2022-02-24 23:00:23 +0000

    blm cleanup

--- blm_cluster.py
+++ blm_cluster.py
@@ -66,24 +66,24 @@ def main(cluster, client):
 
     print('Concat completed')
 
-    # # --------------------------------------------------------------------------------
-    # # Check inputs
-    # # --------------------------------------------------------------------------------
-    # # Inputs file is first argument
-    # with open(os.path.join(ipath), 'r') as stream:
-    #     inputs = yaml.load(stream,Loader=yaml.FullLoader)
+    # --------------------------------------------------------------------------------
+    # Check inputs
+    # --------------------------------------------------------------------------------
+    # Inputs file is first argument
+    with open(os.path.join(ipath), 'r') as stream:
+        inputs = yaml.load(stream,Loader=yaml.FullLoader)
 
-    # # --------------------------------------------------------------------------------
-    # # Read Output directory, work out number of batches
-    # # --------------------------------------------------------------------------------
-    # OutDir = inputs['outdir']
+    # --------------------------------------------------------------------------------
+    # Read Output directory, work out number of batches
+    # --------------------------------------------------------------------------------
+    OutDir = inputs['outdir']
 
-    # # --------------------------------------------------------------------------------
-    # # Clean up files
-    # # --------------------------------------------------------------------------------
-    # os.remove(os.path.join(OutDir, 'nb.txt'))
-    # if os.path.isdir(os.path.join(OutDir, 'tmp')):
-    #     shutil.rmtree(os.path.join(OutDir, 'tmp'))
+    # --------------------------------------------------------------------------------
+    # Clean up files
+    # --------------------------------------------------------------------------------
+    os.remove(os.path.join(OutDir, 'nb.txt'))
+    if os.path.isdir(os.path.join(OutDir, 'tmp')):
+        shutil.rmtree(os.path.join(OutDir, 'tmp'))
     
     print('BLM code complete!')
 
