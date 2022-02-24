64492cfd51a38c74f4a9a5e68b352a3c5d586ed8
Tom Maullin-Sapey <tommaullin@Caustic-Jam.local>
2022-02-24 01:03:34 +0000

    wip

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qsub -V -j $batchIDs -N results -e log/ -o log/ bash /well/nichols/users/inf852/BLMdask/scripts/cluster_blm_concat.sh 2627 /well/nichols/users/inf852/BLMdask/anya.yml
+qsub -V -N results -e /well/nichols/users/inf852/BLMdask/scripts/log/ -o /well/nichols/users/inf852/BLMdask/scripts/log/ bash /well/nichols/users/inf852/BLMdask/scripts/cluster_blm_concat.sh 2627 /well/nichols/users/inf852/BLMdask/anya.yml
