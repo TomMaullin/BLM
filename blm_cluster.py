9f697aa0c979a4fefd05055e24a868bbc974774e
TomMaullin <tommaullin@gmail.com>
2023-03-30 14:52:11 +0100

    wip dask update

--- blm_cluster.py
+++ blm_cluster.py
@@ -1,61 +1,174 @@
-from dask import config
-from dask_jobqueue import SGECluster
-from dask.distributed import Client, as_completed
-from dask.distributed import performance_report
-from lib.blm_setup import setup
-from lib.blm_batch import compute_product_forms
-from lib.blm_concat import combine_batch_masking, combine_batch_designs
-from lib.blm_results import output_results
-from lib.fileio import pracNumVoxelBlocks
-import numpy as np
 import os
 import sys
 import shutil
 import yaml
+import numpy as np
+from lib.blm_setup import setup
+from lib.blm_batch import compute_product_forms
+from lib.blm_concat import combine_batch_masking, combine_batch_designs
+from lib.blm_results import output_results
+from dask import config
+from dask_jobqueue import SLURMCluster
+from dask.distributed import Client, as_completed
+from dask.distributed import performance_report
 
-# Given a dask distributed client run BLM.
-def main(cluster, inputs):
 
+def _main(argv=None):
+    
+    # --------------------------------------------------------------------------------
+    # Check inputs
+    # --------------------------------------------------------------------------------
+    # Inputs file is first argument
+    if len(argv)<1:
+        raise ValueError('Please provide an inputs YAML file.')
+        
+    # Get the inputs filepath
+    else:
+        inputs_yml = argv[0]
+        
+    # Load the inputs yaml file
+    with open(inputs_yml, 'r') as stream:
+        inputs = yaml.load(stream,Loader=yaml.FullLoader)
+    
     # --------------------------------------------------------------------------------
     # Read Output directory, work out number of batches
     # --------------------------------------------------------------------------------
     OutDir = inputs['outdir']
-
+    
     # Get number of nodes
     numNodes = inputs['numNodes']
 
     # Need to return number of batches
     retnb = True
 
+    # --------------------------------------------------------------------------------
+    # Set up cluster
+    # --------------------------------------------------------------------------------
+    if 'clusterType' in inputs:
+
+        # Check if we are using a HTCondor cluster
+        if inputs['clusterType'].lower() == 'htcondor':
+
+            # Load the HTCondor Cluster
+            from dask_jobqueue import HTCondorCluster
+            cluster = HTCondorCluster()
+
+        # Check if we are using an LSF cluster
+        elif inputs['clusterType'].lower() == 'lsf':
+
+            # Load the LSF Cluster
+            from dask_jobqueue import LSFCluster
+            cluster = LSFCluster()
+
+        # Check if we are using a Moab cluster
+        elif inputs['clusterType'].lower() == 'moab':
+
+            # Load the Moab Cluster
+            from dask_jobqueue import MoabCluster
+            cluster = MoabCluster()
+
+        # Check if we are using a OAR cluster
+        elif inputs['clusterType'].lower() == 'oar':
+
+            # Load the OAR Cluster
+            from dask_jobqueue import OARCluster
+            cluster = OARCluster()
+
+        # Check if we are using a PBS cluster
+        elif inputs['clusterType'].lower() == 'pbs':
+
+            # Load the PBS Cluster
+            from dask_jobqueue import PBSCluster
+            cluster = PBSCluster()
+
+        # Check if we are using an SGE cluster
+        elif inputs['clusterType'].lower() == 'sge':
+
+            # Load the SGE Cluster
+            from dask_jobqueue import SGECluster
+            cluster = SGECluster()
+
+        # Check if we are using a SLURM cluster
+        elif inputs['clusterType'].lower() == 'slurm':
+
+            # Load the SLURM Cluster
+            from dask_jobqueue import SLURMCluster
+            cluster = SLURMCluster()
+
+        # Check if we are using a local cluster
+        elif inputs['clusterType'].lower() == 'local':
+
+            # Load the Local Cluster
+            from dask.distributed import LocalCluster
+            cluster = LocalCluster()
+
+        # Raise a value error if none of the above
+        else:
+            raise ValueError('The cluster type, ' + inputs['clusterType'] + ', is not recognized.')
+
+    else:
+        # Raise a value error if the cluster type was not specified
+        raise ValueError('Please specify "clusterType" in the inputs yaml.')
+    
+    # --------------------------------------------------------------------------------
+    # Connect to client
+    # --------------------------------------------------------------------------------
+    
     # Connect to cluster
     client = Client(cluster)   
+
+    # --------------------------------------------------------------------------------
+    # Run Setup
+    # --------------------------------------------------------------------------------
 
     # Ask for a node for setup
     cluster.scale(1)
 
     # Get number of batches
-    future_0 = client.submit(setup, inputs_yml, retnb, pure=False)
-    nb = future_0.result()
+    future_s = client.submit(setup, inputs_yml, retnb, pure=False)
+    nb = future_s.result()
 
-    del future_0
-
-    # MARKER: ASK USER ABOUT PREVIOUS OUTPUT
+    # Delete the future object (NOTE: This is important! If you don't delete this dask
+    # tries to rerun it every time you call the result function again, e.g. after each
+    # stage of the pipeline).
+    del future_s
+    
+    # --------------------------------------------------------------------------------
+    # Run Batch Jobs
+    # --------------------------------------------------------------------------------
 
     # Ask for numNodes nodes for BLM batch
     cluster.scale(numNodes)
 
-    # Futures list
-    futures = client.map(compute_product_forms, *[np.arange(nb)+1, [inputs_yml]*nb], pure=False)
 
-    # results
-    results = client.gather(futures)
-    del futures, results
+    # Ask for a node for setup
+    cluster.scale(numNodes)
 
-    print('Batches completed')
+    # Empty futures list
+    futures = []
 
-    # --------------------------------------------------------
-    # CONCATENATE NUMBER OF OBSERVATIONS AND DESIGNS
-    # --------------------------------------------------------
+    # Submit jobs
+    for i in np.arange(1,nb+1):
+
+        # Run the jobNum^{th} job.
+        future_b = client.submit(compute_product_forms, i, inputs_yml, pure=False)
+
+        # Append to list 
+        futures.append(future_b)
+
+    # Completed jobs
+    completed = as_completed(futures)
+
+    # Wait for results
+    for i in completed:
+        i.result()
+
+    # Delete the future objects (NOTE: see above comment in setup section).
+    del i, completed, futures, future_b
+
+    # --------------------------------------------------------------------------------
+    # Run Concatenation Job
+    # --------------------------------------------------------------------------------
 
     # Batch jobs
     maskJob = False
@@ -108,9 +221,9 @@ def main(cluster, inputs):
 
     del future_b_first, res
 
-    # --------------------------------------------------------
-    # RESULTS
-    # --------------------------------------------------------
+    # --------------------------------------------------------------------------------
+    # Run Results Jobs
+    # --------------------------------------------------------------------------------
 
     # Number of jobs for results (practical number of voxel batches)
     pnvb = int(np.minimum(numNodes, pracNumVoxelBlocks(inputs)))
@@ -136,14 +249,14 @@ def main(cluster, inputs):
 
     del i, completed, futures, future_c
 
+    # MARKER UP TO HERE
+
     # --------------------------------------------------------------------------------
     # Clean up files
     # --------------------------------------------------------------------------------
     if os.path.isfile(os.path.join(OutDir, 'nb.txt')):
         os.remove(os.path.join(OutDir, 'nb.txt'))
-    # if os.path.isdir(os.path.join(OutDir, 'tmp')):
-    #     shutil.rmtree(os.path.join(OutDir, 'tmp'))
-   
+
     print('BLM analysis complete!')
     print('')
     print('---------------------------------------------------------------------------')
@@ -153,41 +266,5 @@ def main(cluster, inputs):
     # Close the client
     client.close()
 
-# If running this function
 if __name__ == "__main__":
-
-    # Inputs file is first argument
-    if len(sys.argv)>1:
-        if not os.path.split(sys.argv[1])[0]:
-            inputs_yml = os.path.join(os.getcwd(),sys.argv[1])
-        else:
-            inputs_yml = sys.argv[1]
-    else:
-        inputs_yml = os.path.join(os.path.realpath(__file__),'blm_config.yml')
-
-    # Read in inputs
-    with open(inputs_yml, 'r') as stream:
-        inputs = yaml.load(stream,Loader=yaml.FullLoader)
-
-    # Timeouts
-    config.set(distributed__comm__timeouts__tcp='90s')
-    config.set(distributed__comm__timeouts__connect='90s')
-    config.set(scheduler='single-threaded')
-    config.set({'distributed.scheduler.allowed-failures': 50}) 
-    config.set(admin__tick__limit='3h')
-
-    # Specify cluster setup
-    cluster = SGECluster(cores=1,
-                         memory="100GB",
-                         queue='short.qc',
-                         walltime='01:00:00',
-                         interface="ib0",
-                         local_directory="/well/nichols/users/inf852/BLMdask/",
-                         log_directory="/well/nichols/users/inf852/BLMdask/log/",
-                         silence_logs=True,
-                         scheduler_options={'dashboard_address': ':8889'})
-
-
-    # Run BLM
-    main(cluster, inputs)
-
+    _main(sys.argv[1:])
