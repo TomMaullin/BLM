14c4c4d0bc95abd7d3d459ea83fc62978abfdc2a
TomMaullin <tommaullin@gmail.com>
2023-03-30 15:44:20 +0100

    dask update wip

--- blm_cluster.py
+++ blm_cluster.py
@@ -225,6 +225,10 @@ def _main(argv=None):
     # Number of jobs for results (practical number of voxel batches)
     pnvb = int(np.minimum(numNodes, pracNumVoxelBlocks(inputs)))
 
+    print(pnvb, nb)
+    with open(os.path.join(OutDir,"tmp.txt"), "w") as f:
+        f.write(pnvb,nb)
+
     # Empty futures list
     futures = []
 
